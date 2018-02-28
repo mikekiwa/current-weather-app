cdfe410000b01a6ca66dd8cb1c7314d6a5a739a9
Ashwin V Prabhu <ashwinvprabhu@icloud.com>
2018-02-27 23:46:55 -0800

    changed api calls to open weather api from http to https

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_KEY = 'ea073b821469e03b1db555d7e6053a75'
-const URL = `http://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`
+const URL = `https://api.openweathermap.org/data/2.5/weather?appid=${API_KEY}`
 
 export const FETCH_WEATHER_DATA = 'FETCH_WEATHER_DATA'
 
